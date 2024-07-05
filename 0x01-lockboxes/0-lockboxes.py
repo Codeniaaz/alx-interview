110b158bae26ef385c7badd259c80fee610326e6
codeniaaz <muktarabdiaziz12@gmail.com>
2024-07-05 06:03:02 +0300

    0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
+"""
+This module provides a function to determine if all boxes can be unlocked.
+"""
+
 def canUnlockAll(boxes):
+    """
+    Determines if all boxes can be unlocked starting from the first box (box 0).
+
+    Args:
+        boxes (list of list of int): A list where each element is a list of keys contained in a box.
+
+    Returns:
+        bool: True if all boxes can be opened, else False.
+    """
     n = len(boxes)
     opened = [False] * n
     opened[0] = True
