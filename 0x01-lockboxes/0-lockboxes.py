7493b99010eca4bd7bf0699ad0840b333d244f5a
codeniaaz <muktarabdiaziz12@gmail.com>
2024-07-05 06:00:08 +0300

    0-lockboxes.py

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def canUnlockAll(boxes):
+    n = len(boxes)
+    opened = [False] * n
+    opened[0] = True
+    stack = [0]
+    
+    while stack:
+        current_box = stack.pop()
+        
+        for key in boxes[current_box]:
+            if 0 <= key < n and not opened[key]:
+                opened[key] = True
+                stack.append(key)
+    
+    return all(opened)
